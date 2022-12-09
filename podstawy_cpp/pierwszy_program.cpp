db0d7c0f8efbe64d754a6058cd49408f5c2db2e4
Grzegorz Bąk <gbak@student.agh.edu.pl>
2022-12-09 15:58:55 +0100

    pierwszy program w c++

---
+++ podstawy_cpp/pierwszy_program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    cout << "Hello World!" << endl;
+
+    string imie;
+
+    cout << "Witaj! Podaj swoje imie: ";
+    cin >> imie;
+
+    cout << endl << "Witaj " << imie <<"! " << endl;
+
+  return 0;
+}

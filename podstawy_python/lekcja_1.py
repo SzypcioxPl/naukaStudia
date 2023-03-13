fc28219d80c9ee9723e31887cf867334f83410c7
Grzegorz Bąk <bakg@student.agh.edu.pl>
2023-03-13 17:57:13 +0100

    podstawy pythona lekcje 1-5

---
+++ podstawy_python/lekcja_1.py
@@ -0,0 +1,14 @@
+# pierwszy program w języku Python
+
+print("Hello World!!!")
+print(5 + 27)
+print(5 - 27)
+print(5 / 27)
+print(5 % 2)
+
+# komentarz
+
+zmienna = "Witaj "
+
+imie = input("Podaj imię: ")
+print(zmienna + imie + " !")
